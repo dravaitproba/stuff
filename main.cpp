4e91d66d5129af9d41013e15bc4ef981f2cd4f0d
tibor dravai <dravait@freemail.hu>
2011-12-25 12:09:19 +0100

    added 4. line

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Hello world!" << endl;
     cout << "Hello world!  2x" << endl;
     cout << "Hello world!  3x" << endl;
+    cout << "Hello world!  4x" << endl;
     return 0;
 }
